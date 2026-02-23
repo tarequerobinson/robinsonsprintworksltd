e96033ffc00527990dd7c6215f6144a4969c7722
Tareque Robinson <tarequerobinson@gmail.com>
2026-02-23 10:38:01 -0500

    fav icon changes again

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon-dark-32x32.png",
         media: "(prefers-color-scheme: light)",
       },
       {
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon-dark-32x32.png",
         type: "image/svg+xml",
       },
     ],
